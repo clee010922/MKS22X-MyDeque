9ac2d19c8ab638bc72b3b9640de7603951a0b260
Chong Lee <clee010922@gmail.com>
2019-03-24 17:05:10 -0400

    formatting

--- MyDeque.java
+++ MyDeque.java
@@ -6,7 +6,7 @@ public class MyDeque<E> {
   private int size, start, end;
 
   @SuppressWarnings("unchecked")
-  public MyDeque(){
+  public MyDeque() {
     data = (E[])new Object[10];
     size = 0;
     start = 0;
@@ -14,7 +14,7 @@ public class MyDeque<E> {
   }
 
   @SuppressWarnings("unchecked")
-  public MyDeque(int initialCapacity){
+  public MyDeque(int initialCapacity) {
     data = (E[])new Object[initialCapacity];
     size = 0;
     start = 0;
@@ -22,7 +22,7 @@ public class MyDeque<E> {
   }
 
   @SuppressWarnings("unchecked")
-  public void resize(){
+  public void resize() {
     E[] temp = (E[])new Object[data.length * 2 + 1];
     if (start < end) {
       for (int i = start; i <= end; i++) {
@@ -45,11 +45,11 @@ public class MyDeque<E> {
     data = temp;
   }
 
-  public int size(){
+  public int size() {
     return size;
   }
 
-  public String toString(){
+  public String toString() {
     String result = "{";
     if (start < end) {
       for (int i = start; i <= end; i++) {
@@ -68,7 +68,7 @@ public class MyDeque<E> {
     return result;
   }
 
-  public void addFirst(E element){
+  public void addFirst(E element) {
     if (element == null)
       throw new NullPointerException();
     if (size == data.length)
@@ -88,7 +88,7 @@ public class MyDeque<E> {
     size++;
   }
 
-  public void addLast(E element){
+  public void addLast(E element) {
     if (element == null)
       throw new NullPointerException();
     if (size == data.length)
@@ -104,7 +104,7 @@ public class MyDeque<E> {
     size++;
   }
 
-  public E removeFirst(){
+  public E removeFirst() {
     if (data[start] == null)
       throw new NoSuchElementException();
     E temp = data[start];
@@ -116,7 +116,7 @@ public class MyDeque<E> {
     return temp;
   }
 
-  public E removeLast(){
+  public E removeLast() {
     if (data[end] == null)
       throw new NoSuchElementException();
     E temp = data[end];
@@ -128,13 +128,13 @@ public class MyDeque<E> {
     return temp;
   }
 
-  public E getFirst(){
+  public E getFirst() {
     if (data[start] == null)
       throw new NoSuchElementException();
     return data[start];
   }
 
-  public E getLast(){
+  public E getLast() {
     if (data[end] == null)
       throw new NoSuchElementException();
     return data[end];
