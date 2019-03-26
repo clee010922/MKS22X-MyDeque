c6dd0c6062b4223859cd7613d679c898ff03d667
clee010922 <clee01@stuy.edu>
2019-03-26 10:19:09 -0400

    progress in class

--- Calculator.java
+++ Calculator.java
@@ -6,6 +6,15 @@ public class Calculator {
 
     public static double eval(String s) {
       String[] values = s.split(" ");
+      MyDeque deque = new MyDeque(values.length);
+      for (int i = 0; i < values.length; i++) {
+        deque.addFirst(values[i]);
+      }
+      int val1 = Integer.parseInt(deque.getFirst());
+      deque.removeFirst();
+      int val2 = Integer.parseInt(deque.getFirst());
+      deque.removeFirst();
+      if (deque.getFirst)
     }
 
 }
